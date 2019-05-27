8dd7d51255d4a3e4b76dc526c734ece436a55a65
arwiew <pocht1991@yandex.ru>
2019-05-27 22:10:07 +0300

    Загружено ДЗ № 3 (с учетом замечаний с урока)

--- core/src/ru/geekbrains/Screen/MenuScreen.java
+++ core/src/ru/geekbrains/Screen/MenuScreen.java
@@ -3,69 +3,67 @@ package ru.geekbrains.Screen;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 
+
 import ru.geekbrains.Base.BaseScreen;
+import ru.geekbrains.Math.Rect;
+import ru.geekbrains.Sprite.Background;
+import ru.geekbrains.Sprite.IcoMove;
 
 public class MenuScreen extends BaseScreen {
 
- private SpriteBatch batch;
- private Texture img;
- private Texture imgLogo;
- private Vector2 touch;
- private Vector2 v;
- private Vector2 position;
- private Vector2 a,s,d,w;
+
+ private Texture bg;
+ private Background background;
+ private static final float LEN = 0.94f;
+ private Texture im;
+ private IcoMove icoMove;
 
 @Override
 public void show(){
     super.show();
-    batch = new SpriteBatch();
-    img = new Texture("fon.jpg");
-    imgLogo = new Texture("badlogic.jpg");
-    touch = new Vector2();
-    v = new Vector2(3,3); //установили вектор скорости
-    position = new Vector2(0,0); // установили стартовую позицию
-    a = new Vector2(position.x,position.y);
-    s = new Vector2(position.x,position.y);
-    d = new Vector2(position.x,position.y);
-    w = new Vector2(position.x,position.y);
+    bg = new Texture("fon.jpg");
+    im = new Texture("Ship.png");
+    background = new Background(new TextureRegion(bg));
+    icoMove = new IcoMove(new TextureRegion(im));
 }
 
-@Override
+
+    @Override
     public void render(float delta){
     super.render(delta);
+    update(delta);
     Gdx.gl.glClearColor(0.4f, 0.3f, 0.9f, 0.5f); //МОЖНО ЗАДАТЬ ФОН
     Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT); //очистка экрана, работеат в паре с ClaerColor
     batch.begin();
-    //	batch.setColor(0.33f,0.33f,0.33f,0.4f);
-    batch.draw(img, 0, 0,1100,1800);
-    //	batch.setColor(0.33f,0.7f,0.8f,0.5f);
-    batch.draw(imgLogo,touch.x,touch.y,50,50);
+    background.draw(batch);
+    icoMove.draw(batch);
     batch.end();
-}
+  }
 
-@Override
+    private void update(float delta){
+        icoMove.update(delta);
+    }
+
+    @Override
     public void dispose(){
-    batch.dispose();
-    img.dispose();
+    bg.dispose();
+    im.dispose();
     super.dispose();
     }
 
     @Override
-    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-      super.touchDown(screenX,screenY,pointer,button);
-      touch.set(screenX, Gdx.graphics.getHeight()-screenY);
-        System.out.println("tochDown"+touch.x+" "+touch.y);
-        return false;
+    public void resize(Rect worldBounds) {
+        super.resize(worldBounds);
+        background.resize(worldBounds);
+        icoMove.resize(worldBounds);
     }
 
     @Override
-    public boolean keyDown(int keycode) {
-      super.keyDown(keycode);
-      System.out.println(position);
-        return false;
+    public boolean touchDown(Vector2 touch, int pointer) {
+       icoMove.touchDown(touch,pointer);
+       return false;
     }
 }
